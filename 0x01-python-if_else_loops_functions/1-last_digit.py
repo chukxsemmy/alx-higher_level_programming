a99967aca578a794d5f7417c263d5d41dfda478e
Chukxsemmy <chukxsemmy@gmail.com>
2022-08-24 06:07:00 +0800

    value of last digit compared with 5

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 lastDigit = number % 10 if number >= 0 else ((-number % 10) * -1)
+msg = f"Last digit of {number} is {last_digit}"
 
 if lastDigit == 0:
-    print(f"Last digit of {number} is {lastDigit} and is 0")
+    print(f"{msg} and is 0")
 
 elif lastDigit > 5 and lastDigit % 10 != 0:
-    print(f"Last digit of {number} is {lastDigit} and is greater than 5")
+    print(f"{msg} and is greater than 5")
 
 else:
-    print(f"Last digit of {number} is {lastDigit} and is less than 6 and not 0
-")
+    print(f"{msg} and is less than 6 and not 0")
