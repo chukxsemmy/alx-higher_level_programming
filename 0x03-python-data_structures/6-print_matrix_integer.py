5870b38cd817db71cfc6a3a20cf2c24a0293b89f
Chukxsemmy <chukxsemmy@gmail.com>
2022-08-30 09:37:22 +0800

    function that prints matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,28 +1,8 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    len_a = len(tuple_a)
-    len_b = len(tuple_b)
-
-    if len_a == 0:
-        ai = 0
-        aj = 0
-    elif len_a == 1:
-        ai = tuple_a[0]
-        aj = 0
-    else:
-        ai = tuple_a[0]
-        aj = tuple_a[1]
-
-    if len_b == 0:
-        bi = 0
-        bj = 0
-    elif len_b == 1:
-        bi = tuple_b[0]
-        bj = 0
-    else:
-        bi = tuple_b[0]
-        bj = tuple_b[1]
-
-    my_matrix = (ai + bi, aj + bj)
-
-    return (my_matrix)
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end='')
+        print()
