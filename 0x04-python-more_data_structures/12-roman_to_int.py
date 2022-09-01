2621c67edd40a3d2a921d76a23d9ea0e644b5cd6
Chukxsemmy <chukxsemmy@gmail.com>
2022-09-01 12:21:35 +0800

    romman numerals in integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_symbols = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    if (roman_string is None) or (type(roman_string) is not str):
+        return 0
+
+    num = len(roman_string)
+    to_int = roman_symbol[roman_string[num-1]]
+    for i in range(num - 1, 0, -1):
+        insrt_roman_value = roman_symbol[roman_string[i]]
+        real_roman_value = roman_symbol[roman_string[i-1]]
+
+        if real_roman_value >= insrt_roman_value:
+            to_int += real_roman_value
+        else:
+            to_int -= real_roman_value
+    return to_int
