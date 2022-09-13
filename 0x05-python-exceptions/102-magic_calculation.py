e52f594d8c5a3ff973b9323923c7e055587ff9e3
Chukxsemmy <chukxsemmy@gmail.com>
2022-09-13 10:51:09 +0800

    magic calculation

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    magic_cal = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too Far')
+            else:
+                magic_cal += a ** b / i
+        except:
+            magic_cal  = b + a
+            break
+    return (magic_cal)
