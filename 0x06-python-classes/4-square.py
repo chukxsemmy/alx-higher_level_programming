37863b85aae3b542a636c06da44a9e5531cf274a
Chukxsemmy <chukxsemmy@gmail.com>
2022-09-14 09:30:06 +0800

    Area of a square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+module with Class
+"""
+
+
+class Square:
+    """a square class"""
+    def __init__(self, size=0):
+        """constructor"""
+        self.__size = size
+
+    def area(self):
+        """area of Square"""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """get"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """set"""
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
