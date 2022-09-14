8607e089712819a8f1abe48277c6da2f9d262da8
Chukxsemmy <chukxsemmy@gmail.com>
2022-09-14 09:38:11 +0800

    printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,41 +1,30 @@
 #!/usr/bin/python3
+"""class Square that defines a square"""
 
 
-class Square:
-    """Class of Square"""
+class Square():
+    """square class"""
 
     def __init__(self, size=0):
-        """constructor"""
-        if (type(size) is not int):
-            raise (TypeError("size must be an integer"))
-        elif (size < 0):
-            raise (ValueError("size must be >= 0"))
-        else:
-            self.__size = size
-
-    def area(self):
-        """get area of the Square"""
-        return (self.__size ** 2)
-
-    def my_print(self):
-        """print a Square"""
-        if (self.__size == 0):
-            print()
-        else:
-            for rows in range(self.__size):
-                print("#" * self.__size)
+        self.__size = size
 
     @property
     def size(self):
-        """getter"""
-        return (self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value):
-        """setter"""
         if (type(value) is not int):
-            raise (TypeError("size must be an integer"))
+            raise TypeError("size must be an integer")
         elif (value < 0):
-            raise (ValueError("size must be >= 0"))
-        else:
-            self.__size = value
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if not self.__size:
+            print("")
+        for i in range(self.__size):
+            print("#" * self.__size)
