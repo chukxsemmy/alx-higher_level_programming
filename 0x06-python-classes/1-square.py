02373f98de7b59bc330906288f6338aa74bfa2b0
Chukxsemmy <chukxsemmy@gmail.com>
2022-09-14 08:20:05 +0800

    class module

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" a class module in python """
+
+
+class Square():
+    """Class square"""
+
+    def __init__(self, size):
+        """ class initializaton"""
+
+        """Args: 
+        size (int): size of the square
+        """
+        self.__size = size
