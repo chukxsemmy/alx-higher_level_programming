a1b0a6b4eb987960d3eaf484537465e0edd6bdeb
Chukxsemmy <chukxsemmy@gmail.com>
2022-09-28 07:36:32 +0800

    creates a python object

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Python load JSON"""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates Python object from a JSON file."""
+    with open(filename) as f:
+        return json.load(f)
