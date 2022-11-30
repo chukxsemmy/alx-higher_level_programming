9ab178f71acb5f4c93e2229d6a67d3852f641fa5
Chukxsemmy <chukxsemmy@gmail.com>
2022-11-30 10:39:12 +0800

    printing a rectangle with the caracter X, rotates and doubles it

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -9,9 +9,11 @@ module.exports = class Rectangle {
       console.log('X'.repeat(this.width));
     }
   }
+
   rotate () {
     [this.width, this.height] = [this.height, this.width];
   }
+
   double () {
     [this.width, this.height] = [this.width * 2, this.height * 2];
   }
