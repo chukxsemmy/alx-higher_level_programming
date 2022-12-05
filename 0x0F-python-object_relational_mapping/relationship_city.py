882a7abbd600b2245ada68979d0c4702d134c52d
Chukxsemmy <chukxsemmy@gmail.com>
2022-12-05 11:58:11 +0800

    Script that creates the State “California” with the City

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    inherits from Base (imported from model_state)
+    links to the MySQL table cities
+"""
+
+from sqlalchemy.orm import relationship
+from relationship_state import Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    '''City model for my db'''
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False,
+                autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship('State', back_populates='cities')
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Defines a state model that contain the class definition
+ of a State and an instance Base = declarative_base()
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    '''state class for the state table'''
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', back_populates='state',
+                          cascade='all, delete, delete-orphan')
