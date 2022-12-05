d068fae2550d5f37110cf4025ed9bdcb48d3ccfe
Chukxsemmy <chukxsemmy@gmail.com>
2022-12-05 12:07:16 +0800

    fix

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-""" Script that lists all State objects, and corresponding
-    City objects, contained in the database hbtn_0e_101_usa """
+"""
+ lists all State objects, and corresponding City
+ objects, contained in the database hbtn_0e_101_usa
+"""
+
+import sys
+from unicodedata import name
+from venv import create
+from sqlalchemy import create_engine, true
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import Base, City
 
 if __name__ == "__main__":
-
-    import sys
-    from relationship_city import Base, City
-    from relationship_state import State
-    from sqlalchemy.schema import Table
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
 
-    s = Session(engine)
-    for state in s.query(State).order_by(State.id).all():
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
-    s.close()
+            print("\t{}: {}".format(city.id, city.name))
+    session.close()
+    engine.dispose()
